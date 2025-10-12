23e33458803dc34a6ae7f368d2e49051f168214f
Girma <girmaenkuchille@gmail.com>
2025-10-12 20:45:45 +0300

    backend file added with fixing error

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
-import { Module } from "@nestjs/common"
-import { JwtModule } from "@nestjs/jwt"
-import { PassportModule } from "@nestjs/passport"
-import { ConfigModule, ConfigService } from "@nestjs/config"
-import { AuthService } from "./auth.service"
-import { AuthController } from "./auth.controller"
-import { UsersModule } from "../users/users.module"
-import { JwtStrategy } from "./strategies/jwt.strategy"
+import { Module } from "@nestjs/common";
+import { JwtModule, JwtModuleOptions } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
+import { UsersModule } from "../users/users.module";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
   imports: [
@@ -13,13 +13,15 @@ import { JwtStrategy } from "./strategies/jwt.strategy"
     PassportModule.register({ defaultStrategy: "jwt" }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>("JWT_SECRET"),
-        signOptions: {
-          expiresIn: configService.get<string>("JWT_EXPIRATION", "7d"),
-        },
-      }),
       inject: [ConfigService],
+      useFactory: (configService: ConfigService): JwtModuleOptions => {
+        return {
+          secret: configService.get<string>("JWT_SECRET") || "defaultSecret",
+          signOptions: {
+            expiresIn: 3600, 
+          },
+        };
+      },
     }),
   ],
   controllers: [AuthController],
