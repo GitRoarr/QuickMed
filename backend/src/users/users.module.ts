1ef3e9aec5ba48314f04d37d8abde645789ea851
Girma <girmaenkuchille@gmail.com>
2025-11-04 12:34:38 +0300

    module injection

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,13 +1,16 @@
-import { Module } from "@nestjs/common"
-import { TypeOrmModule } from "@nestjs/typeorm"
-import { UsersService } from "./users.service"
-import { UsersController } from "./users.controller"
-import { User } from "./entities/user.entity"
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { User } from "./entities/user.entity";
+import { CloudinaryModule } from "@/profile/cloudinary.module";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    CloudinaryModule,
+  ],
   controllers: [UsersController],
-  
   providers: [UsersService],
   exports: [UsersService],
 })
