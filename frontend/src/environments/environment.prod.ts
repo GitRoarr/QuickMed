d349a76473a992b62e0d95f764eb1a4325d1b3e1
girma <girmaenkuchille@gmail.com>
2026-02-17 13:39:43 +0300

    Update API URL to Render HTTPS backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://52.14.156.138:3000/api",
+  apiUrl: "https://quickmed-backend-jsff.onrender.com/api",
   stripePublishableKey: "pk_test_51SYtbCAAbfECi3qV283FG7a0LQ9HKzLe8OOwf4xVIp0DIdrebEnjc3OAay5G0ixVkl6TLu3KY7i3s45KgPfPB1yw002NHNoYcp",
   supabaseUrl: "https://qqckxwehcgljshkpmmca.supabase.co",
   supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFxY2t4d2VoY2dsanNoa3BtbWNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMDQ3NjksImV4cCI6MjA3OTU4MDc2OX0.pT68AMb3E-2nlMbU3JsChpTB4EadI9Up05M6k6tu-EI",
